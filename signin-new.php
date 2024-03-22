7f84d5c51e624194175de880505e5d779299ada1
88bones <123712642+88bones@users.noreply.github.com>
2024-03-22 14:56:14 +0545

    validation done

--- signin-new.php
+++ signin-new.php
@@ -1,91 +1,89 @@
-<?php 
+<?php
 
 // session_start();
 
 //     include 'connection.php';
 //     include 'functions.php';
-    
 
 
-    //both works to redirect the user to the login page if the user is not logged in
-    // if(!isset($_SESSION["user"])) {
-        // header("Location: signin-new.php");
-    // }
+
+//both works to redirect the user to the login page if the user is not logged in
+// if(!isset($_SESSION["user"])) {
+// header("Location: signin-new.php");
+// }
 
 //start the session
 session_start();
 
-    include 'connection.php';
-    include 'functions.php';
+include 'connection.php';
+include 'functions.php';
 
-    //check if the user is alreadylogged in, redirect to home page if true
-    // if(isset($_SESSION["username"])) {
-        // header("Location: home.php");
-        // exit;
-    // }
+//check if the user is alreadylogged in, redirect to home page if true
+// if(isset($_SESSION["username"])) {
+// header("Location: home.php");
+// exit;
+// }
 
-    //check if the form is submitted
-    //     if($_SERVER['REQUEST_METHOD'] == 'POST') {
-       
-    //         //something was posted
-    //         $username = $_POST['username'];
-    //         // $email = $_POST['email'];
-    //         $password = $_POST['password'];
+//check if the form is submitted
+//     if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    //         //read from database
-    //         $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-    //         $result = mysqli_query($conn, $sql);
+//         //something was posted
+//         $username = $_POST['username'];
+//         // $email = $_POST['email'];
+//         $password = $_POST['password'];
 
-    //         if($result) {
-    //             //set session variables
-    //             $_SESSION["username"] = $username;
-    //             // $_SESSION["email"] = $email;
-    //             // $_SESSION["role"] = "admin";
-    //             header("Location: home.php");
-    //             exit;
-    //         } else {
-    //             header("Location: signin-new.php?error=invalid");
-    //             exit;
-    //         }
-    //     }
+//         //read from database
+//         $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+//         $result = mysqli_query($conn, $sql);
 
-    // check if the form is submitted
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-       
-        // if(isset($name) || isset($username) || isset($email) || isset($password)) {
+//         if($result) {
+//             //set session variables
+//             $_SESSION["username"] = $username;
+//             // $_SESSION["email"] = $email;
+//             // $_SESSION["role"] = "admin";
+//             header("Location: home.php");
+//             exit;
+//         } else {
+//             header("Location: signin-new.php?error=invalid");
+//             exit;
+//         }
+//     }
 
-            //something was posted
-            $username = $_POST['username'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
+// check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-            if(!empty($username) && !is_numeric($username) &&!empty($email) && !empty($password)) {
+    // if(isset($name) || isset($username) || isset($email) || isset($password)) {
 
-                //read from database
-                $sql = "SELECT * FROM users WHERE username = '$username' AND email = '$email'";
-                $result = mysqli_query($conn, $sql);
+    //something was posted
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-                if($result) {
+    if (!empty($username) && !is_numeric($username) && !empty($email) && !empty($password)) {
 
-                    if($result &&  mysqli_num_rows($result) > 0) {
+        //read from database
+        $sql = "SELECT * FROM users WHERE username = '$username' AND email = '$email'";
+        $result = mysqli_query($conn, $sql);
 
-                        $user_data = mysqli_fetch_assoc($result);
+        if ($result) {
 
-                        if($user_data['password'] === $password) {
+            if ($result &&  mysqli_num_rows($result) > 0) {
 
-                            $_SESSION['userid'] = $user_data['userid'];
-                            header('Location: home.php');
-                            die;
-                        }
-                    }
-                }    
-                
-            
-            } else {
-                echo "Please enter some valid information";
+                $user_data = mysqli_fetch_assoc($result);
+
+                if ($user_data['password'] === $password) {
+
+                    $_SESSION['userid'] = $user_data['userid'];
+                    header('Location: home.php');
+                    die;
+                }
             }
-        // }
+        }
+    } else {
+        echo "Please enter some valid information";
     }
+    // }
+}
 
 ?>
 
@@ -93,19 +91,21 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign-In</title>
-    
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    
-    <link rel="stylesheet" href="signin-new.css">
-    
+
+    <link rel="stylesheet" href="signin-new.css?v=<?php echo time(); ?>">
+
     <script src="https://kit.fontawesome.com/4591222058.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
 
     <div class="container">
@@ -113,7 +113,7 @@ session_start();
         <div class="form-box">
             <h1 id="title">Sign In</h1>
             <form action="" method="POST">
-                <div class="input-group">                   
+                <div class="input-group">
                     <div class="input-field">
                         <i class="fa-solid fa-circle-user"></i>
                         <input type="text" id="text" name="username" class="form-control" placeholder="Username" required>
@@ -126,9 +126,10 @@ session_start();
 
                     <div class="input-field">
                         <i class="fa-solid fa-key"></i>
-                        <input type="password" id="text" name="password" class="form-control" placeholder="Password"required>
+                        <input type="password" id="text" name="password" class="form-control" placeholder="Password" required>
                     </div>
 
+                    <p class="lol">Don't have and account? Click <a href="signup-new-1.php">Here</a></p>
                 </div>
                 <div class="btn-field">
                     <button type="submit" name="login" class="disable" id="signinBtn">Sign In</button>
@@ -138,4 +139,5 @@ session_start();
     </div>
 
 </body>
+
 </html>
--- signup-new-1.php
+++ signup-new-1.php
@@ -2,131 +2,133 @@
 session_start();
 
 
-    include 'connection.php';
-    include 'functions.php';
-    
+include 'connection.php';
+include 'functions.php';
 
-    //both works to redirect the user to the login page if the user is not logged in
-    // if(!isset($_SESSION["user"])) {
-    //     header("Location: signin-new.php");
-    // }
+
+//both works to redirect the user to the login page if the user is not logged in
+// if(!isset($_SESSION["user"])) {
+//     header("Location: signin-new.php");
+// }
 
 // session_start();    
 
-    // include 'connection.php';
-    // include 'functions.php';
-    
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+// include 'connection.php';
+// include 'functions.php';
 
-        //something was posted
-        $name = $_POST['name'];
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $confirm_password = $_POST['confirm_password'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        $password_hash = password_hash($password, PASSWORD_DEFAULT);
+    //something was posted
+    $name = $_POST['name'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
 
-        if(!empty($name) && !empty($username) && !is_numeric($username) && !empty($email) && !empty($password) && !empty($confirm_password) && ($password == $confirm_password)) {
+    $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
-            //save to database
-            $sql = "INSERT INTO `users`(`name`, `username`, `email`, `password`) VALUES('$name', '$username','$email', '$password')";
-            mysqli_query($conn, $sql);
-            header('Location: signin-new.php');
-            die;
-            // exit;
+    if (!empty($name) && !empty($username) && !is_numeric($username) && !empty($email) && !empty($password) && !empty($confirm_password) && ($password == $confirm_password)) {
 
-        } else {
-            echo "Please enter some valid information";
-        }
+        //save to database
+        $sql = "INSERT INTO `users`(`name`, `username`, `email`, `password`) VALUES('$name', '$username','$email', '$password')";
+        mysqli_query($conn, $sql);
+        header('Location: signin-new.php');
+        die;
+        // exit;
 
-        // $errors = array();
-
-        // if(empty($name) OR !empty($username) OR !empty($email) OR !empty($password) OR !empty($confirm_password)){
-        //     array_push($errors, "All fields are ");
-        // }
-        // if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        //     array_push($errors, "Please enter a valid email address");
-        // }
-        // if(strlen($password)<8) {
-        //     array_push($errors, "Password must be at least 8 characters long");
-        // }
-        // if($password!== $confirm_password) {
-        //     array_push($errors, "Passwords do not match");
-        // } 
-        // if(count($errors) > 0) {
-        //     foreach($errors as $error) {
-        //         echo "<div class='alert alert-danger'>$error</div>";
-        //     }
-        // } else {
-        //     require_once 'connection.php';
-        //     //save to database
-        //      $sql = "INSERT INTO `users`(`name`, `username`, `email`, `password`) VALUES('$name', '$username','$email', '$password')";
-        //      $stmt = mysqli_stmt_init($conn);
-        //      $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
-        //      if($prepareStmt) {
-        //         mysqli_stmt_bind_param($stmt,"sss",$name,$username,$email,$password_hash);
-        //         mysqli_stmt_execute($stmt);
-        //         echo "<div class='alert alert-success'>You have registered successfully.</div>";
-        //      } else {
-        //         die;
-        //      }
-            //  mysqli_query($conn, $sql);
-            //  header('Location: signin-new.php');
-            //  die;
-             // exit;
-        // } 
+    } else {
+        echo "Please enter some valid information";
     }
-        
-    
+
+    // $errors = array();
+
+    // if(empty($name) OR !empty($username) OR !empty($email) OR !empty($password) OR !empty($confirm_password)){
+    //     array_push($errors, "All fields are ");
+    // }
+    // if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    //     array_push($errors, "Please enter a valid email address");
+    // }
+    // if(strlen($password)<8) {
+    //     array_push($errors, "Password must be at least 8 characters long");
+    // }
+    // if($password!== $confirm_password) {
+    //     array_push($errors, "Passwords do not match");
+    // } 
+    // if(count($errors) > 0) {
+    //     foreach($errors as $error) {
+    //         echo "<div class='alert alert-danger'>$error</div>";
+    //     }
+    // } else {
+    //     require_once 'connection.php';
+    //     //save to database
+    //      $sql = "INSERT INTO `users`(`name`, `username`, `email`, `password`) VALUES('$name', '$username','$email', '$password')";
+    //      $stmt = mysqli_stmt_init($conn);
+    //      $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
+    //      if($prepareStmt) {
+    //         mysqli_stmt_bind_param($stmt,"sss",$name,$username,$email,$password_hash);
+    //         mysqli_stmt_execute($stmt);
+    //         echo "<div class='alert alert-success'>You have registered successfully.</div>";
+    //      } else {
+    //         die;
+    //      }
+    //  mysqli_query($conn, $sql);
+    //  header('Location: signin-new.php');
+    //  die;
+    // exit;
+    // } 
+}
+
+
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign-Up</title>
-    
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    
+
     <link rel="stylesheet" href="signup-new-1.css">
-    
+
     <script src="https://kit.fontawesome.com/4591222058.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
 
     <div class="container">
         <div class="form-box">
             <h1 id="title">Sign Up</h1>
-            <form action="" class="" method="POST">
+            <form action="" class="" method="POST" onsubmit="return validation()">
                 <div class="input-group">
                     <div class="input-field" id="nameField">
                         <i class="fa-solid fa-user"></i>
-                        <input type="text" id="" name="name" class="form-control" placeholder="Name" >
+                        <input type="text" id="name" name="name" class="form-control" placeholder="Name">
                     </div>
-                
+
                     <div class="input-field">
                         <i class="fa-solid fa-circle-user"></i>
-                        <input type="text" id="" name="username" class="form-control" placeholder="Username" >
-                    </div> 
+                        <input type="text" id="username" name="username" class="form-control" placeholder="Username">
+                    </div>
 
                     <div class="input-field">
                         <i class="fa-solid fa-envelope"></i>
-                        <input type="email" id="" name="email" class="form-control" placeholder="Email" >
+                        <input type="email" id="email" name="email" class="form-control" placeholder="Email">
                     </div>
 
                     <div class="input-field">
                         <i class="fa-solid fa-key"></i>
-                        <input type="password" id="" name="password" class="form-control" placeholder="Password" >
+                        <input type="password" id="password" name="password" class="form-control" placeholder="Password">
                     </div>
 
                     <div class="input-field" id="cpassField">
                         <i class="fa-solid fa-key"></i>
-                        <input type="password" id="" name="confirm_password" class="form-control" placeholder="Confirm Password" >
+                        <input type="password" id="repassword" name="confirm_password" class="form-control" placeholder="Confirm Password">
                     </div>
 
                     <p>Already have an account? Click <a href="signin-new.php">Here</a></p>
@@ -138,6 +140,46 @@ session_start();
             </form>
         </div>
     </div>
+    <script>
+        function validation() {
+            let error = 0;
+            const nameRegex = /^[a-zA-Z ]+$/;
+            const userNameRegex = /^[a-zA-Z0-9_]+$/;
+            const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+           // const phoneRegex = /^\d{10}$/;
+            const emailRegex = /^[^\s@]+@[^s@]+\.[^/s@]+$/;
+
+            const name = document.getElementById('name').value;
+            const uname = document.getElementById('usernamename').value;
+            const pass = document.getElementById('password').value;
+            const email = document.getElementById('email').value;
+
+            if (!nameRegex.test(name)) {
+                document.getElementById('nameError').style = 'display: block';
+                error++;
+            }
+            if (!passwordRegex.test(passwordRegex)) {
+                document.getElementById('passError').style = 'display: block';
+                alert('Invalid password');
+                error++;
+                //console.log("hello");
+            }
+            if (!userNameRegex.test(uname)) {
+                document.getElementById('unameError').style = 'display: block';
+                error++;
+            }
+
+            if (!emailRegex.test(email)) {
+                document.getElementById('emailError').style = 'display: block';
+                error++;
+            }
+            if (error != 0) {
+                return false;
+                //preventDefault();
+            }
+        }
+    </script>
 
 </body>
+
 </html>
