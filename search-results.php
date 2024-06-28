75223614846fcacf087c9aa91ad349784d4e46aa
88bones <123712642+88bones@users.noreply.github.com>
2024-06-28 12:40:37 +0545

    v1.0

--- search-results.php
+++ search-results.php
@@ -4,7 +4,7 @@ include 'menu.php';
 
 $search = isset($_GET['search-input']) ? $conn->real_escape_string($_GET['search-input']) : '';
 
-$sql = "SELECT * FROM bikes WHERE bname LIKE '%$search%'";
+$sql = "SELECT * FROM bikes WHERE bname LIKE '$search%'";
 $result = $conn->query($sql);
 
 $items = [];
@@ -27,74 +27,48 @@ $conn->close();
 </head>
 
 <body>
-    <!--<h1 class="search-header">Search Results for "<?php echo htmlspecialchars($search); ?>"</h1>
-    <div class="grid-container">
-        <?php if (count($items) > 0) : ?>
-            <?php foreach ($items as $item) : ?>
-                <div class="grid-item">
+    <?php if (count($items) > 0) : ?>
+        <div class="bike-display">
+            <div class="bike-container">
+                <div class="bike-image">
                     <?php
+                    $item = $items[0];
                     if (!empty($item['image'])) {
-                        echo '<td><img src="' . ($item['image']) . '" width="350"></td>';
+                        echo '<img src="' . htmlspecialchars($item['image']) . '" width="500px">';
                     } else {
-                        echo '<td>No image</td>';
-                    }; ?>
-                    <h2><?php echo htmlspecialchars($item['bname']); ?></h2>
-                    <p><?php echo htmlspecialchars($item['brand']); ?></p>
+                        echo '<p>No image</p>';
+                    }
+                    ?>
                 </div>
-            <?php endforeach; ?>
-        <?php else : ?>
-            <p>No items found.</p>
-        <?php endif; ?>
-    </div>-->
-
-    <div class="bike-display">
-        <div class="bike-container">
-            <div class="bike-image">
-                <?php
-                if (count($items) > 0)
-                    foreach ($items as $item);
-                if (!empty($item['image'])) {
-                    echo '<td><img src="' . ($item['image']) . '" width="500px"></td>';
-                } else {
-                    echo '<td>No image</td>';
-                };
-                ?>
+                <div class="bike-specs">
+                    <p class="bike-brand"><?php echo htmlspecialchars($item['brand'] . ' ' . $item['bname']); ?></p>
+                </div>
             </div>
-            <div class="bike-specs">
-                <p class="bike-brand"><?php echo htmlspecialchars($item['brand']) ?>
-                    <?php echo htmlspecialchars($search); ?></p>
+        </div>
+        <div class="key-specs">
+            <h3>Key Specifications</h3>
+        </div>
+        <div class="bike-details">
+            <div class="bike-info">
+                <table>
+                    <tr>
+                        <th>Price</th>
+                        <td><?php echo htmlspecialchars($item['price']); ?></td>
+                    </tr>
+                    <tr>
+                        <th>Type</th>
+                        <td><?php echo htmlspecialchars($item['btype']); ?></td>
+                    </tr>
+                    <tr>
+                        <th>Engine CC</th>
+                        <td><?php echo htmlspecialchars($item['enginecc']); ?></td>
+                    </tr>
+                </table>
             </div>
-
-
         </div>
-
-    </div>
-    <div class="key-specs">
-        <h3>Key Specifications</h3>
-    </div>
-    <div class="bike-details">
-        <div class="bike-info">
-            <table>
-                <tr>
-                    <th>Price</th>
-
-                    <td><?php echo htmlspecialchars($item['price']) ?></td>
-                </tr>
-                <tr>
-                    <th>Type</th>
-
-                    <td><?php echo htmlspecialchars($item['btype']) ?></td>
-                </tr>
-                <tr>
-                    <th>Price</th>
-
-                    <td><?php echo htmlspecialchars($item['enginecc']) ?></td>
-                </tr>
-            </table>
-        </div>
-    </div>
-
-
+    <?php else : ?>
+        <p>No items found.</p>
+    <?php endif; ?>
 </body>
 
 </html>
